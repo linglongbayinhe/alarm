21dac582422c3d8e42094ccf8538c41db8da4641
linglongbayinhe <linglongbayinhe@gmail.com>
2026-04-20 17:45:25 +0800

    update weather icons

---
+++ main/ui/styles.h
@@ -0,0 +1,19 @@
+#ifndef EEZ_LVGL_UI_STYLES_H
+#define EEZ_LVGL_UI_STYLES_H
+
+#include <lvgl/lvgl.h>
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+// Style: time_label
+lv_style_t *get_style_time_label_MAIN_DEFAULT();
+void add_style_time_label(lv_obj_t *obj);
+void remove_style_time_label(lv_obj_t *obj);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /*EEZ_LVGL_UI_STYLES_H*/
